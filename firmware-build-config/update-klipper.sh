ddb8846d64fe24aaf1cf3595ae28adae8cb6691a
Raspberry Pi Zero 2 <pi@rpi-zero.local>
2025-01-08 17:32:45 +0300

    Autocommit from 2025-01-08 17:32:45
    Klipper version: v0.12.0-405-g8a3d2afd
    
    Moonraker version: v0.9.3-4-ga4604e3
    
    Mainsail version: v2.13.2

--- firmware-build-config/update-klipper.sh
+++ firmware-build-config/update-klipper.sh
@@ -27,13 +27,27 @@ function flash_board() {
   make --directory "$klipper_path" flash FLASH_DEVICE="$1"
 }
 
+function update_config() {
+  python_code="
+import kconfiglib
+import sys
+a = kconfiglib.Kconfig(filename=sys.argv[1])
+a.load_config(filename=sys.argv[2],replace=False)
+a.write_config(filename=sys.argv[2],save_old=True)
+"
+  pushd "$klipper_path"
+  PYTHONPATH=lib/kconfiglib python3 -c "$python_code" src/Kconfig $1
+  popd
+}
+
 configs_dir="$(realpath $(dirname "$0"))"
 
 for config_name in "${!boards[@]}"; do
   serial_path="/dev/serial/by-id/usb-Klipper_${boards[$config_name]}"
   serial_katapult_path="/dev/serial/by-id/usb-katapult_${boards[$config_name]}"
   config_path="$configs_dir/$config_name"
-  echo $config_path
+  echo "Update board config: $config_path"
+  update_config "$config_path"
   if ! [[ ( -e "$serial_path" || -e "$serial_katapult_path") &&  -e "$config_path" ]]; then
     echo "Serial path or config path were not found! Skipping ${config_name}..."
     continue
